94a8eec22fbba7dbf762a6c21125bb1750ab00bd
Isak <isakulstrup@gmail.com>
2025-01-17 16:27:32 +0100

    map loading experiment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import { Elm } from "./src/Main.elm";
 Elm.Main.init({ node: document.getElementById("app") });
 
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const map = urlParams.get('map')
+
+
+const response = await fetch(`public/${map}.json`);
+const file = await response.json();
+console.log("map file", file);
