6051a23723fe09b7775d67eb6fac180754d8adfb
Isak <isakulstrup@gmail.com>
2025-01-23 17:24:43 +0100

    better error handling

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ import { Elm } from "./src/Main.elm";
 const app = Elm.Main.init({ node: document.getElementById("app") });
 
 app.ports.requestChunk.subscribe(function (chunk) {
-    console.log("request chunk:", chunk);
+    // console.log("request chunk:", chunk);
+
     fetch(`map/${chunk}.json`)
-        .then((response) => response.json())
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Something went wrong');
+        })
         .then((json) => {
-            console.log("sending chunk", chunk, json);
+            // console.log("sending chunk", chunk, json);
             app.ports.gotChunk.send(json);
+        })
+        .catch((error) => {
+            console.log("Chunk not found:", chunk)
         });
 });
