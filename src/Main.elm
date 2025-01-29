76d86d59fd6a7196d70f3d3b9536f7d18b8002bf
Isak <isakulstrup@gmail.com>
2025-01-29 18:52:06 +0100

    cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ import Browser
 import Browser.Events
 import Engine.Codec as Codec
 import Engine.Grid as Grid exposing (Grid)
-import Engine.Path exposing (Path)
+import Engine.Path
 import Engine.Point as Point exposing (Point)
 import Engine.Render as Render exposing (Camera)
 import File.Download as Download
